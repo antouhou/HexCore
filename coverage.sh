e73c2c47a6e0e27c533d1713fa43c540cab630e1
Anton Suprunchuk <antouhou@gmail.com>
2018-06-16 15:09:59 +0300

    Fixed coverage script, added profiler, coverae and test output to gitignore

--- coverage.sh
+++ coverage.sh
@@ -1,10 +1,11 @@
 # Run tests and generate coverage
-PROFILER_PROFILE="log:coverage,covfilter-file=coverage.filter"
-TESTS_EXECUTABLE="Tests/bin/Debug/Tests.exe"
-mono --debug --profile=$PROFILER_PROFILE $TESTS_EXECUTABLE --result=nunit-results.xml
+NUNIT_RESULTS=nunit-results.xml
+PROFILER_PROFILE=log:coverage,covfilter-file=coverage.filter
+TESTS_EXECUTABLE=Tests/bin/Debug/Tests.exe
+mono --debug --profile=$PROFILER_PROFILE $TESTS_EXECUTABLE --result=$NUNIT_RESULTS
 
 # Transform results to junit format
-saxonb-xslt TestResult.xml nunit3-junit.xslt > junit-results.xml
+saxonb-xslt $NUNIT_RESULTS nunit3-junit.xslt > junit-results.xml
 
 # Saving coverage output:
 mprof-report --reports=coverage --out=coverage.report output.mlpd
