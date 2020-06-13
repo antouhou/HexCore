2602ad9dfdf4bc84154caff504ed762c0bdf378f
Anton Suprunchuk <antouhou@gmail.com>
2020-06-13 20:23:40 +0300

    Add different usage message to publish script

--- HexCore/publish.sh
+++ HexCore/publish.sh
@@ -1,7 +1,11 @@
 VERSION=$1
 API_KEY=$2
-[[ -z "$1" ]] && { echo "Parameter 1 should be a version to publish" ; exit 1; }
-[[ -z "$2" ]] && { echo "Parameter 2 should be the API key" ; exit 1; }
+MISSING_OPTIONS_MESSAGE="
+Invalid arguments!\n
+Usage: ./publish.sh %version_to_publish% %nuget_key%
+"
+[[ -z "$1" ]] && { echo $MISSING_OPTIONS_MESSAGE; exit 1; }
+[[ -z "$2" ]] && { echo $MISSING_OPTIONS_MESSAGE ; exit 1; }
 
 dotnet pack ./HexCore.csproj --configuration Release
 dotnet nuget push ./bin/Release/HexCore.$VERSION.nupkg -k $API_KEY -s https://api.nuget.org/v3/index.json
