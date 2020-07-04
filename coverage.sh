1a597a3dcb02134c36dc49780576b115ea1994ae
Anton Suprunchuk <antouhou@gmail.com>
2020-07-04 22:44:25 +0300

    Move coverage file one level up

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='./coverage/lcov.info'
+dotnet test --no-restore --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage/lcov.info'
