91cca6851a5c3df45eb6666afe79f2d05de6940c
Anton Suprunchuk <antouhou@gmail.com>
2020-07-05 00:32:12 +0300

    Remove coveralls step from publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 API_KEY=$2
-GITHUB_API_KEY=$3
+PACKAGES_API_KEY=$3
 MISSING_OPTIONS_MESSAGE="
 Invalid arguments!\n
 Usage: ./publish.sh %version_to_publish% %nuget_key% %GitHub_packages_key%
@@ -12,5 +12,5 @@ Usage: ./publish.sh %version_to_publish% %nuget_key% %GitHub_packages_key%
 [[ -z "$3" ]] && { echo $MISSING_OPTIONS_MESSAGE ; exit 1; }
 
 dotnet pack ./HexCore/HexCore.csproj --configuration Release
+PACKAGES_API_KEY=${PACKAGES_API_KEY} dotnet nuget push ./HexCore/bin/Release/HexCore.$VERSION.nupkg -s "github"
 dotnet nuget push ./HexCore/bin/Release/HexCore.$VERSION.nupkg -k ${API_KEY} -s "nuget.org"
-GITHUB_API_KEY=${GITHUB_API_KEY} dotnet nuget push ./HexCore/bin/Release/HexCore.$VERSION.nupkg -s "github"
