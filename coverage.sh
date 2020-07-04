a058b61a10ef8806704ce8cea57a3db1447136fb
Anton Suprunchuk <antouhou@gmail.com>
2020-07-04 22:21:51 +0300

    Add restore from nuget.org

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,2 @@
-# Run tests and generate coverage
-NUNIT_RESULTS=nunit-results.xml
-PROFILER_PROFILE=log:coverage,covfilter-file=coverage.filter
-TESTS_EXECUTABLE=Tests/bin/Debug/Tests.exe
-mono --debug --profile=$PROFILER_PROFILE $TESTS_EXECUTABLE --result=$NUNIT_RESULTS
-
-# Transform results to junit format
-saxonb-xslt $NUNIT_RESULTS nunit3-junit.xslt > junit-results.xml
-
-# Saving coverage output:
-mprof-report --reports=coverage --out=coverage.report output.mlpd
+#!/usr/bin/env bash
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov
